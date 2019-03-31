7e6bb15e93273aab88e706acff66563c9118b3e3
ArdeshirV <ArdeshirV@protonmail.com>
2019-04-01 01:14:44 +0430

    Add files

---
+++ hello_world.adb
@@ -0,0 +1,23 @@
+with Ada.Text_IO;
+--------------------------------------------------------------------------------
+procedure hello_world is
+  intValue: integer := 0;
+  ------------------------------------------------------------------------------
+  function factorial(num: integer)return integer is
+    intResult: integer;
+  begin
+    intResult := 1;
+
+    if num > 1 then
+      intResult := factorial(num - 1)* num;
+    end if;
+
+    return intResult;
+  end factorial;
+  ------------------------------------------------------------------------------
+begin
+  intValue := 9;
+  Ada.Text_IO.Put("Factorial(" & Integer'Image(intValue) & " ) = ");
+  Ada.Text_IO.Put_Line( Integer'Image(factorial(intValue)) );
+end hello_world;
+--------------------------------------------------------------------------------
---
+++ makefile
@@ -0,0 +1,3 @@
+hello_world:	hello_world.adb
+	gnat make hello_world.adb
+	rm -f *~ *.o *.ali
